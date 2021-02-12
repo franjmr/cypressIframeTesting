75be9417b9d1d6ab4b5c0f26f7c069e4b2eba203
fjmoranromero <fjmoranromero@cegid.com>
2021-02-12 12:29:42 +0100

    Add pending spec

---
+++ cypress/integration/checkout.is.pending_spec.ts
@@ -0,0 +1,112 @@
+describe('Aplazame - Checkout PENDING', () => {
+    it('should visit Aplazame', () => {
+        cy.visit('https://demo.aplazame.com');
+        cy.get('article').should('be.visible')
+    })
+
+    it('should display pay type widget title in iframe', {
+        retries: {
+          runMode: 2,
+          openMode: 1
+        }
+      }, () => {
+        cy.enter('iframe', { timeout: 10000 }).then(getBody => {
+            getBody().find('.aplazame-widget-smart-title').should('be.visible')
+            getBody().find('.aplazame-widget-smart-title').should('have.text','¡Págalo a plazos!')
+        })
+    })
+
+    it('should click button to pay', () => {
+        cy.get('button.pay-with-aplazame').as('buttonPay')
+        cy.get('@buttonPay').click()
+    })
+
+    it('should accept and submit checkout form', {
+        retries: {
+          runMode: 2,
+          openMode: 1
+        }
+      }, () => {
+        cy.enter('#aplazame-checkout-iframe', { timeout: 10000 }).then(getBody => {
+            getBody().find('form[name="checkout"]').as('formCheckout')
+            cy.get('@formCheckout').should('be.visible')
+            cy.get('@formCheckout').find('input[name=accepts_gdpr]').click()
+            cy.get('@formCheckout').find('button[type=submit]').click()
+        })
+    })
+
+    it('should fill personal data and submit customer form', {
+        retries: {
+          runMode: 2,
+          openMode: 1
+        }
+      }, () => {
+        cy.enter('#aplazame-checkout-iframe', { timeout: 10000 }).then(getBody => {
+            getBody().find('form[name="checkout"]').as('formCheckout')
+            cy.get('@formCheckout').should('be.visible')
+            cy.get('@formCheckout').find('input[name=document_id]').click().clear().type('99999995C')
+            cy.get('@formCheckout').find('input[name=birthday]').click().clear().type('14011984', {delay: 100})
+            cy.get('@formCheckout').find('input[type="checkbox"]').click()
+            cy.get('@formCheckout').find('button[type=submit]').click()
+        })
+    })
+    
+    it('should submit customer form', {
+        retries: {
+          runMode: 2,
+          openMode: 1
+        }
+      }, () => {
+        cy.enter('#aplazame-checkout-iframe', { timeout: 10000 }).then(getBody => {
+            getBody().find('form[name="checkout"]').as('formCheckout')
+            cy.get('@formCheckout').submit()
+        })
+    })
+
+    it("should accept the credit", {
+        retries: {
+          runMode: 2,
+          openMode: 1
+        }
+      }, () => {
+        cy.enter('#aplazame-checkout-iframe', { timeout: 10000 }).then(getBody => {
+            getBody().find('#aplazame----otp----signature').as('otpSignature')
+            cy.get('@otpSignature').find('.-sms-sent', {timeout: 10000}).should('contain.text','Te hemos enviado un PIN al')
+            cy.get('@otpSignature').find('#OtpSecureContainer', {timeout: 10000}).should('be.visible')
+        })
+    })
+
+    it("should fill One Time Password", {
+        retries: {
+          runMode: 2,
+          openMode: 1
+        }
+      }, () => {
+        cy.enter('#aplazame-checkout-iframe', { timeout: 10000 }).then(getBody => {
+            getBody().find('#sandbox').as('sandbox').should('be.visible')
+            getBody().find('#OtpSecureInput').as('optSecureInput').should('be.visible')
+            cy.get('@sandbox').invoke('text').then( (text: string) => {
+                const sandboxNumber = text.match(/\d/g);
+                return sandboxNumber.join('')
+            }).then( sandboxNumber => {
+                cy.get('@optSecureInput').clear().click().type(sandboxNumber, {delay: 100})
+            })
+        })
+    })
+
+    
+    it("should verify identity to approve funding", {
+        retries: {
+          runMode: 2,
+          openMode: 1
+        }
+      }, () => {
+        cy.enter('#aplazame-checkout-iframe', { timeout: 10000 }).then(getBody => {
+            getBody().find('modal-upload-documentation').as('modalUploadDocumentation').should('be.visible')
+            cy.get('@modalUploadDocumentation').should('contain.text','Verifica tu identidad')
+            cy.get('@modalUploadDocumentation').find('form[name=challenge_document_id]').should('exist')
+            cy.get('@modalUploadDocumentation').find('#drop-front-area').should('be.visible')
+            cy.get('@modalUploadDocumentation').find('#drop-back-area').should('be.visible')
+        })
+    })
+})
